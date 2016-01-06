c0fae7db26689cd132dc6715ca19f29e88c8a834
karolcircle <serwisnet1@gmail.com>
2016-01-06 19:35:54 +0100

    Restrict access to project actions to admins only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+  before_action :authorize_admin!, except: [:index, :show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -45,12 +48,28 @@ flash[:notice] = "Project has been destroyed."
 redirect_to projects_path
 end
 
+
+
 private
+def set_project
+@project = Project.find(params[:id])
+rescue ActiveRecord::RecordNotFound
+flash[:alert] = "The project you were looking" +
+" for could not be found."
+redirect_to projects_path
+end
 
 def project_params
   params.require(:project).permit(:name, :description)
 end
 
+def authorize_admin!
+require_signin!
+unless current_user.admin?
+flash[:alert] = "You must be an admin to do that."
+redirect_to root_path
+end
+end
 
 
 end
--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 feature "Editing Projects" do
 before do
-FactoryGirl.create(:project, name: "TextMate 2")
+  sign_in_as!(FactoryGirl.create(:admin_user))
+
 visit "/"
 click_link "TextMate 2"
 click_link "Edit Project"
